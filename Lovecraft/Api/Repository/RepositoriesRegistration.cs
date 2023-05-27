0105b1121ae00814a0238e3071ff3b1ae38e0754
Alexandre Levy <alexandre.l@tokywoky.com>
2023-05-27 03:08:51 +0200

    new Repository for storyvote + new way to register all repository

---
+++ Lovecraft/Api/Repository/RepositoriesRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Lovecraft.Api.Model;
+
+namespace Lovecraft.Api.Repository;
+
+public class RepositoriesRegistration
+{
+	public static void RegisterRepositories(IServiceCollection services)
+	{
+		services.AddTransient<IUserRepository, UserRepository>();
+		services.AddTransient<ICommonRepository<Team>, TeamRepository>();
+		services.AddTransient<ICommonRepository<UserTeam>, UserTeamRepository>();
+		services.AddTransient<ICommonRepository<Story>, StoryRepository>();
+		services.AddTransient<ICommonRepository<Page>, PageRepository>();
+		services.AddTransient<ICommonRepository<Event>, EventRepository>();
+		services.AddTransient<ICommonRepository<StoryVote>, StoryVoteRepository>();
+	}
+}
---
+++ Lovecraft/Api/Repository/StoryVoteRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+using Lovecraft.Api.Model;
+
+namespace Lovecraft.Api.Repository;
+
+public class StoryVoteRepository : ICommonRepository<StoryVote>
+{
+	public IQueryable<StoryVote> GetAll(int? page, Expression<Func<StoryVote, bool>>? whereExpression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public StoryVote? GetById(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public StoryVote Add(StoryVote entity)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void Update(StoryVote entity)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void Delete(StoryVote id)
+	{
+		throw new NotImplementedException();
+	}
+}
