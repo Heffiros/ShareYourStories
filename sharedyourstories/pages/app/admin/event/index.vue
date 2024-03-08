dfca9ac0d973bb5f800aeb8e7c92e5de9bbadda0
Alexandre Levy <vasax.levy@gmail.com>
2024-03-09 00:25:49 +0100

    Event Feed
    Event admin fedd

--- sharedyourstories/pages/app/admin/event/index.vue
+++ sharedyourstories/pages/app/admin/event/index.vue
@@ -1,28 +1,45 @@
 <template>
-    <v-container class="bg-surface-variant">
-      Hello Event
-    </v-container>
-  </template>
-  
-  <script>
-  export default {
-    layout: 'admin',
-    components: {
-      
-    },
-    data() {
-      return {
-        page: 0
-      }
-    },
-    computed: {
-    },
-    methods: {
-    },
-    mounted () {
-      this.$store.dispatch('events/FETCH_EVENTS', { page: this.page, mode: 'full' })
+  <div>
+    <v-row>
+      <v-col class="text-right">
+        <v-col cols="auto">
+          <v-btn
+            size="small"
+            color="info"
+            @click="goToEventCreation"
+            >
+            Créer un événement
+          </v-btn>
+        </v-col>
+      </v-col>
+    </v-row>
+
+    <app-event-feed />
+  </div>
+</template>
+
+<script>
+import createStory from '~/components/popups/CreateStory'
+import AppEventFeed from '~/components/AppEventFeed'
+
+export default {
+  components: {
+    createStory,
+    AppEventFeed
+  },
+  data () {
+    return {
+      page: 0
+    }
+  },
+  mounted () {
+    this.$store.dispatch('events/RESETS_EVENT')
+  },
+  methods: {
+    goToEventCreation () {
+      this.$router.push('/app/admin/event/add')
     }
   }
-  </script>
-  
-  <style>
+}
+</script>
+
