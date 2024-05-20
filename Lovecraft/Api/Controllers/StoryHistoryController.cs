e11f0995cf19be240ff848254d522b0816be5856
Alexandre Levy <vasax.levy@gmail.com>
2024-05-21 01:39:30 +0200

    post & put storyHistory controller

--- Lovecraft/Api/Controllers/StoryHistoryController.cs
+++ Lovecraft/Api/Controllers/StoryHistoryController.cs
@@ -1,6 +1,7 @@
 using Lovecraft.Api.Model;
 using Lovecraft.Api.Model.PublicApi;
 using Lovecraft.Api.Repository;
+using Lovecraft.Model;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Security.Claims;
@@ -20,9 +21,65 @@ namespace Lovecraft.Api.Controllers
 		}
 
 		[HttpPost]
-		public ActionResult Post([FromQuery] string search)
+		public ActionResult Post([FromBody] PublicApi_StoryHistoryModel model)
 		{
+            var userIdClaim = HttpContext.User.FindFirstValue("userId");
+            if (userIdClaim == null)
+            {
+                return BadRequest();
+            }
+
+			User user = _luow.Users.GetById(Int32.Parse(userIdClaim));
+			if (user == null || user.Id != Int32.Parse(userIdClaim))
+            {
+				NotFound();
+			}
+
+			if (_luow.StoryHistories.GetAll().Any(sh => sh.UserId == model.UserId && sh.StoryId == model.StoryId))
+			{
+                return Conflict("Resource already exists.");
+            }
+
+			StoryHistory storyHistory = new StoryHistory
+			{
+				UserId = model.UserId,
+				StoryId = model.StoryId,
+				LastPageReadId = model.LastPageReadId,
+				Reread = 0,
+				Date = DateTime.UtcNow,
+				HistoryState = StoryHistory.State.Reading
+			};
+
+			_luow.StoryHistories.Add(storyHistory);
+			_luow.Save();
 			return Ok();
 		}
-	}
+
+        [HttpPut]
+        public ActionResult Put([FromBody] PublicApi_StoryHistoryModel model)
+        {
+            var userIdClaim = HttpContext.User.FindFirstValue("userId");
+            if (userIdClaim == null)
+            {
+                return BadRequest();
+            }
+
+            StoryHistory storyHistoryToUpdate = _luow.StoryHistories.GetById(model.Id);
+            if (storyHistoryToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            storyHistoryToUpdate.UserId = model.UserId;
+            storyHistoryToUpdate.StoryId = model.StoryId;
+            storyHistoryToUpdate.LastPageReadId = model.LastPageReadId;
+            storyHistoryToUpdate.Reread = model.Reread;
+            storyHistoryToUpdate.Date = DateTime.UtcNow;
+            storyHistoryToUpdate.HistoryState = StoryHistory.State.Reading;            
+
+            _luow.StoryHistories.Update(storyHistoryToUpdate);
+            _luow.Save();
+            return Ok();
+        }
+    }
 }
