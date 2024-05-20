3045475b4be5d623134ba282056bcfda955afa73
Heffiros <vasax.levy@gmail.com>
2024-05-20 14:51:08 +0200

    some design

--- sharedyourstories/components/AppBadgeFeed.vue
+++ sharedyourstories/components/AppBadgeFeed.vue
@@ -1,49 +1,55 @@
 <template>
-  <div>
-    <div
-      v-for="(badge, index) in badges"
-      :key="badge.id" 
-      :class="{ 'badge-card': index !== 0, 'block-inline': true }"
-    >
-      <app-badge :badge="badge" />
-    </div>
-  </div>  
+  <v-container>
+    <v-card class="badge-panel">
+      <v-card-title class="badge-title">
+        Badges
+      </v-card-title>
+      <v-card-subtitle class="badge-subtitle">
+        Découvre ce qu'il te reste à débloquer
+      </v-card-subtitle>
+      <v-divider></v-divider>
+      <div class="badge-content">
+        <v-row>
+          <v-col v-for="(badge, index) in badges" :key="badge.id" cols="12" sm="6">
+            <app-badge :badge="badge" />
+          </v-col>
+        </v-row>
+      </div>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 import AppBadge from '~/components/AppBadge'
 
 async function fetch(context) {
-    let data = { page: context.page }
-    await context.$store.dispatch('badges/FETCH_BADGES', data)
-    const currentNbBadges = context.badges.length
-    if (currentNbBadges % 5 != 0) {
-      context.hasMore = false
-    }
-    context.page++
+  let data = { page: context.page }
+  await context.$store.dispatch('badges/FETCH_BADGES', data)
+  const currentNbBadges = context.badges.length
+  if (currentNbBadges % 5 != 0) {
+    context.hasMore = false
   }
+  context.page++
+}
 
 export default {
   components: {
     AppBadge
   },
-  data () {
+  data() {
     return {
-        page: 0 
+      page: 0
     }
   },
   computed: {
-    currentUser () {
+    currentUser() {
       return this.$auth.user
     },
-    badges () {
-        return this.$store.state.badges.badges
-      }
+    badges() {
+      return this.$store.state.badges.badges
+    }
   },
-  methods: {
-    
-  },
-  mounted () {
+  mounted() {
     this.$store.dispatch('badges/RESET_BADGES')
     this.page = 0
     fetch(this)
@@ -51,13 +57,29 @@ export default {
 }
 </script>
 
-<style>
-.block-inline {
-  display: inline-block;
-  margin-top: 16px;
+<style scoped>
+.badge-panel {
+  background-color: #3d4242;
+  border-radius: 8px;
+  padding: 16px;
+  max-height: 500px;
+  overflow-y: auto;
 }
 
-.badge-card {
-  margin-left: 32px;
+.badge-title {
+  font-size: 24px;
+  text-align: center;
+  width: 100%;
+}
+
+.badge-subtitle {
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  width: 100%;
+}
+
+.badge-content {
+  padding-top: 16px;
 }
 </style>
