08a2d7384fbace0c5d1fa59ff29e9dbf8f8b979a
optium <kevinvalat@gmail.com>
2025-10-01 22:42:28 +0200

    button notification

---
+++ systories/app/components/sections/Notification.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex gap-1">
+    <NotificationButton :icon="Bell" :showPastille="true" />
+    <NotificationButton :icon="Moon" />
+    <NotificationButton :icon="MessageSquare" :showPastille="true" />
+  </div>
+</template>
+<script setup lang="ts">
+  import NotificationButton from '~/components/common/NotificationButton.vue'
+  import { Bell, Moon, MessageSquare } from "lucide-vue-next"
+</script>
